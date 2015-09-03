a8ac465471559c9dff776ac2f098423a4cae37d4
TotallyAnxious <jentrefzger@gmail.com>
2015-09-03 01:29:04 -0400

    Update cpufreq_dancedance.c

--- drivers/cpufreq/cpufreq_dancedance.c
+++ drivers/cpufreq/cpufreq_dancedance.c
@@ -27,18 +27,19 @@
  * It helps to keep variable names smaller, simpler
  */
 
-#define DEF_FREQUENCY_UP_THRESHOLD		(95)
-#define DEF_FREQUENCY_DOWN_THRESHOLD		(40)
+#define DEF_FREQUENCY_UP_THRESHOLD		(90)
+#define DEF_FREQUENCY_DOWN_THRESHOLD		(30)
 #define MIN_SAMPLING_RATE_RATIO			(2)
-#define MIN_SAMPLING_RATE                       10000
-#define DEF_SAMPLING_RATE                       15000
 
 static unsigned int min_sampling_rate;
 
-#define LATENCY_MULTIPLIER			(500)
-#define MIN_LATENCY_MULTIPLIER			(20)
+#define LATENCY_MULTIPLIER			(1000)
+#define MIN_LATENCY_MULTIPLIER			(100)
 #define DEF_SAMPLING_DOWN_FACTOR		(1)
 #define MAX_SAMPLING_DOWN_FACTOR		(10)
+#define MICRO_FREQUENCY_UP_THRESHOLD		(95)
+#define MICRO_FREQUENCY_DOWN_THRESHOLD		(30)
+#define MICRO_FREQUENCY_MIN_SAMPLE_RATE		(10000)
 #define TRANSITION_LATENCY_LIMIT		(10 * 1000 * 1000)
 
 static void do_dbs_timer(struct work_struct *work);
@@ -81,6 +82,8 @@ struct cpu_dbs_info_s {
 };
 static DEFINE_PER_CPU(struct cpu_dbs_info_s, cs_cpu_dbs_info);
 
+DECLARE_PER_CPU(struct cpuidle_device *, cpuidle_devices);
+
 static unsigned int dbs_enable;	/* number of CPUs using this policy */
 
 /*
@@ -105,41 +108,8 @@ static struct dbs_tuners {
 	.down_threshold = DEF_FREQUENCY_DOWN_THRESHOLD,
 	.sampling_down_factor = DEF_SAMPLING_DOWN_FACTOR,
 	.ignore_nice = 0,
-	.freq_step = 3,
+	.freq_step = 10,
 };
-
-static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu,
-						  u64 *wall)
-{
-    u64 idle_time;
-    u64 cur_wall_time;
-    u64 busy_time;
-
-    cur_wall_time = jiffies64_to_cputime64(get_jiffies_64());
-
-    busy_time  = kcpustat_cpu(cpu).cpustat[CPUTIME_USER];
-    busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SYSTEM];
-    busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_IRQ];
-    busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SOFTIRQ];
-    busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_STEAL];
-    busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_NICE];
-
-    idle_time = cur_wall_time - busy_time;
-    if (wall)
-	*wall = jiffies_to_usecs(cur_wall_time);
-
-    return jiffies_to_usecs(idle_time);
-}
-
-static inline cputime64_t get_cpu_idle_time(unsigned int cpu, cputime64_t *wall)
-{
-    u64 idle_time = get_cpu_idle_time_us(cpu, wall);
-
-    if (idle_time == -1ULL)
-	return get_cpu_idle_time_jiffy(cpu, wall);
-
-    return idle_time;
-}
 
 /* keep track of frequency transitions */
 static int
@@ -159,7 +129,7 @@ dbs_cpufreq_notifier(struct notifier_block *nb, unsigned long val,
 
 	/*
 	 * we only care if our internally tracked freq moves outside
-	 * the 'valid' ranges of freqency available to us otherwise
+	 * the 'valid' ranges of frequency available to us otherwise
 	 * we do not change it
 	*/
 	if (this_dbs_info->requested_freq > policy->max
@@ -259,33 +229,34 @@ static ssize_t store_down_threshold(struct kobject *a, struct attribute *b,
 static ssize_t store_ignore_nice_load(struct kobject *a, struct attribute *b,
 				      const char *buf, size_t count)
 {
-	unsigned int input;
-	int ret;
+    unsigned int input;
+    int ret;
 
-	unsigned int j;
+    unsigned int j;
 
-	ret = sscanf(buf, "%u", &input);
-	if (ret != 1)
-		return -EINVAL;
+    ret = sscanf(buf, "%u", &input);
+    if (ret != 1)
+	return -EINVAL;
 
-	if (input > 1)
-		input = 1;
+    if (input > 1)
+	input = 1;
 
-	if (input == dbs_tuners_ins.ignore_nice) /* nothing to do */
-		return count;
-
-	dbs_tuners_ins.ignore_nice = input;
-
-	/* we need to re-evaluate prev_cpu_idle */
-	for_each_online_cpu(j) {
-		struct cpu_dbs_info_s *dbs_info;
-		dbs_info = &per_cpu(cs_cpu_dbs_info, j);
-		dbs_info->prev_cpu_idle = get_cpu_idle_time(j,
-						&dbs_info->prev_cpu_wall);
-		if (dbs_tuners_ins.ignore_nice)
-		      dbs_info->prev_cpu_nice = kcpustat_cpu(j).cpustat[CPUTIME_NICE];
-	}
+    if (input == dbs_tuners_ins.ignore_nice) { /* nothing to do */
 	return count;
+    }
+    dbs_tuners_ins.ignore_nice = input;
+
+    /* we need to re-evaluate prev_cpu_idle */
+    for_each_online_cpu(j) {
+	struct cpu_dbs_info_s *dbs_info;
+	dbs_info = &per_cpu(cs_cpu_dbs_info, j);
+	dbs_info->prev_cpu_idle = get_cpu_idle_time(j,
+						   &dbs_info->prev_cpu_wall, 0);
+	if (dbs_tuners_ins.ignore_nice)
+	    dbs_info->prev_cpu_nice = kcpustat_cpu(j).cpustat[CPUTIME_NICE];
+
+    }
+    return count;
 }
 
 static ssize_t store_freq_step(struct kobject *a, struct attribute *b,
@@ -362,20 +333,23 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 
 		j_dbs_info = &per_cpu(cs_cpu_dbs_info, j);
 
-		cur_idle_time = get_cpu_idle_time(j, &cur_wall_time);
+		cur_idle_time = get_cpu_idle_time(j, &cur_wall_time, 0);
 
-		wall_time = (unsigned int) (cur_wall_time - j_dbs_info->prev_cpu_wall);
+		wall_time = (unsigned int)
+			(cur_wall_time - j_dbs_info->prev_cpu_wall);
 		j_dbs_info->prev_cpu_wall = cur_wall_time;
 
-		idle_time = (unsigned int) (cur_idle_time - j_dbs_info->prev_cpu_idle);
+		idle_time = (unsigned int)
+			(cur_idle_time - j_dbs_info->prev_cpu_idle);
+
 		j_dbs_info->prev_cpu_idle = cur_idle_time;
 
 		if (dbs_tuners_ins.ignore_nice) {
-			cputime64_t cur_nice;
+			u64 cur_nice;
 			unsigned long cur_nice_jiffies;
 
-		        cur_nice = kcpustat_cpu(j).cpustat[CPUTIME_NICE] -
-             			j_dbs_info->prev_cpu_nice;
+			cur_nice = kcpustat_cpu(j).cpustat[CPUTIME_NICE] -
+					 j_dbs_info->prev_cpu_nice;
 			/*
 			 * Assumption: nice time between sampling periods will
 			 * be less than 2^32 jiffies for 32 bit sys
@@ -383,10 +357,9 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 			cur_nice_jiffies = (unsigned long)
 					cputime64_to_jiffies64(cur_nice);
 
-         		j_dbs_info->prev_cpu_nice = kcpustat_cpu(j).cpustat[CPUTIME_NICE];
-			idle_time += jiffies_to_usecs(cur_nice_jiffies);
+			j_dbs_info->prev_cpu_nice = kcpustat_cpu(j).cpustat[CPUTIME_NICE];
+			
 		}
-
 		if (unlikely(!wall_time || wall_time < idle_time))
 			continue;
 
@@ -426,17 +399,8 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 		return;
 	}
 
-	/*
-	 * The optimal frequency is the frequency that is the lowest that
-	 * can support the current CPU usage without triggering the up
-	 * policy. To be safe, we focus 10 points under the threshold.
-	 */
-	if (max_load < (dbs_tuners_ins.down_threshold - 10)) {
-		freq_target = (dbs_tuners_ins.freq_step * policy->max) / 100;
-
-		this_dbs_info->requested_freq -= freq_target;
-		if (this_dbs_info->requested_freq < policy->min)
-			this_dbs_info->requested_freq = policy->min;
+	/* Check for frequency decrease */
+	if (max_load < (dbs_tuners_ins.down_threshold)) {
 
 		/*
 		 * if we cannot reduce the frequency anymore, break out early
@@ -444,8 +408,14 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 		if (policy->cur == policy->min)
 			return;
 
+		freq_target = (dbs_tuners_ins.freq_step * policy->max) / 100;
+
+		this_dbs_info->requested_freq -= freq_target;
+		if (this_dbs_info->requested_freq < policy->min)
+			this_dbs_info->requested_freq = policy->min;
+		
 		__cpufreq_driver_target(policy, this_dbs_info->requested_freq,
-				CPUFREQ_RELATION_H);
+				CPUFREQ_RELATION_L);
 		return;
 	}
 }
@@ -533,13 +503,14 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 			j_dbs_info = &per_cpu(cs_cpu_dbs_info, j);
 			j_dbs_info->cur_policy = policy;
 
-			j_dbs_info->prev_cpu_idle = get_cpu_idle_time(j,
-						&j_dbs_info->prev_cpu_wall);
-			if (dbs_tuners_ins.ignore_nice) {
-				j_dbs_info->prev_cpu_nice =
-						kcpustat_cpu(j).cpustat[CPUTIME_NICE];
-			}
-		}
+	    j_dbs_info->prev_cpu_idle = get_cpu_idle_time(j,
+							  &j_dbs_info->prev_cpu_wall, 0);
+	    if (dbs_tuners_ins.ignore_nice) {
+		j_dbs_info->prev_cpu_nice =
+		    kcpustat_cpu(j).cpustat[CPUTIME_NICE];
+	    }
+	}
+		this_dbs_info->cpu = cpu;
 		this_dbs_info->down_skip = 0;
 		this_dbs_info->requested_freq = policy->cur;
 
@@ -563,13 +534,12 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 				return rc;
 			}
 
-			/*
-			 * conservative does not implement micro like ondemand
-			 * governor, thus we are bound to jiffes/HZ
-			 */
-                        min_sampling_rate = MIN_SAMPLING_RATE;
-                        /* Bring kernel and HW constraints together */
-                        dbs_tuners_ins.sampling_rate = DEF_SAMPLING_RATE;
+			/* Bring kernel and HW constraints together */
+			min_sampling_rate = max(min_sampling_rate,
+					MIN_LATENCY_MULTIPLIER * latency);
+			dbs_tuners_ins.sampling_rate =
+				max(min_sampling_rate,
+				    latency * LATENCY_MULTIPLIER);
 
 			cpufreq_register_notifier(
 					&dbs_cpufreq_notifier_block,
@@ -614,6 +584,7 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 			__cpufreq_driver_target(
 					this_dbs_info->cur_policy,
 					policy->min, CPUFREQ_RELATION_L);
+		dbs_check_cpu(this_dbs_info);
 		mutex_unlock(&this_dbs_info->timer_mutex);
 
 		break;
@@ -622,7 +593,28 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 }
 
 static int __init cpufreq_gov_dbs_init(void)
-{
+{ 
+	cputime64_t idle_time;
+	int cpu = get_cpu();
+
+	idle_time = get_cpu_idle_time_us(cpu, NULL);
+	put_cpu();
+	if (idle_time != -1ULL) {
+		/* Idle micro accounting is supported. Use finer thresholds */
+		dbs_tuners_ins.up_threshold = MICRO_FREQUENCY_UP_THRESHOLD;
+		dbs_tuners_ins.down_threshold = MICRO_FREQUENCY_DOWN_THRESHOLD;
+		/*
+		 * In nohz/micro accounting case we set the minimum frequency
+		 * not depending on HZ, but fixed (very low). The deferred
+		 * timer might skip some samples if idle/sleeping as needed.
+		*/
+		min_sampling_rate = MICRO_FREQUENCY_MIN_SAMPLE_RATE;
+	} else {
+		/* For correct statistics, we need 10 ticks for each measure */
+		min_sampling_rate =
+			MIN_SAMPLING_RATE_RATIO * jiffies_to_usecs(10);
+	}
+	
 	return cpufreq_register_governor(&cpufreq_gov_dancedance);
 }
 
