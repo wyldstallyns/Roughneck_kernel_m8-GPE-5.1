74c062658f5548737e6829aa61330615b0f2d50b
TotallyAnxious <jentrefzger@gmail.com>
2015-09-12 02:14:32 -0400

    Update cpufreq_ondemand.c

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -61,6 +61,8 @@ static unsigned int min_sampling_rate;
 #define LATENCY_MULTIPLIER			(1000)
 #define MIN_LATENCY_MULTIPLIER			(100)
 #define TRANSITION_LATENCY_LIMIT		(10 * 1000 * 1000)
+#define BOOST_DURATION_US			(1000000)
+#define BOOST_FREQ_VAL				(1497600)
 
 #define POWERSAVE_BIAS_MAXLEVEL			(1000)
 #define POWERSAVE_BIAS_MINLEVEL			(-1000)
